fd52f74adb8fff72b39aa4ea27b42d83784a0efa
Kayke Galdiano <kaykegaldiano@gmail.com>
2023-09-11 13:18:14 -0300

    creating checkout page and components

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/checkout', function () {
+    return view('checkout.index');
+})->middleware(['guest'])->name('checkout');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
