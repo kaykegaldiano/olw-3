8fff1463f4a68c58b114c01bcd5b739558fd11b0
Kayke Galdiano <kaykegaldiano@gmail.com>
2023-09-15 17:55:09 -0300

    creating payment config

---
+++ config/payment.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'mercado_pago' => [
+        'public_key' => env('VITE_MERCADO_PAGO_PUBLIC_KEY'),
+        'access_token' => env('MERCADO_PAGO_ACCESS_TOKEN'),
+        'buyer_email' => env('MERCADO_PAGO_BUYER_EMAIL'),
+    ]
+];
