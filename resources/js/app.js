9fe134d28d3dd9f27277ab392f0ace8904d2fb6d
Kayke Galdiano <kaykegaldiano@gmail.com>
2023-09-16 21:37:08 -0300

    instanciando sdk mercado pago

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import checkout from './checkout/checkout';
 
+Alpine.data('checkout', checkout)
 Livewire.start();
